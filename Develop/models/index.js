5a19fa7a6863afb9e83d7825f784dff4cfa151c6
Michael Andrews <michael.andrews@pega.com>
2022-10-03 17:35:18 -0400

    Updated routes, modified model files and server.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,7 +1,7 @@
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
-const Tag = require('../../../models/Tag');
+const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
